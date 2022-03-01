f3e0613629bd7c0939e72e0d0ec0035b9471d3a1
akerimbaeva <akerimbaeva@gmail.com>
2022-03-01 22:32:22 +0600

    complete all tests

--- src/index.js
+++ src/index.js
@@ -1,38 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  let brackets = Object.fromEntries(bracketsConfig);
-  let openBrackets = [];
-  let sameBrackets = '';
-  let len = bracketsConfig.length - 1;
- 
-  for (let i = 0; i<=len; i++) {
-      openBrackets.push(bracketsConfig[i][0]) 
-     }
-    let stack = [];
+    let bracketsPair = Object.fromEntries(bracketsConfig);
+    let closeBrackets = []
+    let openBrackets = [];
+    let len = bracketsConfig.length - 1;
+   
+    for (let i = 0; i<=len; i++) {
+        openBrackets.push(bracketsConfig[i][0]) 
+        closeBrackets.push(bracketsConfig[i][1])
+       }
+
+      let stack = [];
     
-    function getKeyByValue(object, value) {
-            return Object.keys(object).find(key => object[key] === value);
-            }
-    for ( i = 0; i < str.length; i++) {
-            if (getKeyByValue(brackets, str[i]) === brackets[str[i]]) {
-                    sameBrackets = sameBrackets + str[i]
-                } else if ((openBrackets.includes(str[i]))) {
-                stack.push(str[i])
-            } else {
-                if (stack.length === 0) {
-                    return false
-                }
-
-                let topElement = stack[stack.length - 1]
-                
-        
-            if (getKeyByValue(brackets, str[i]) === topElement) {
-                stack.pop();
-            } else {
-                return false
-            }
-            }
-             
-        }
-
-        return (stack.length === 0)&&(sameBrackets.length%2 === 0)
-    }
+      function getKeyByValue(object, value) {
+              return Object.keys(object).find(key => object[key] === value);
+              }
+      for ( i = 0; i < str.length; i++) {
+          if (closeBrackets.includes(str[i])){
+             if (getKeyByValue(bracketsPair, str[i])  === stack[stack.length-1]) {
+                 stack.pop()
+             } else {
+                 stack.push(str[i])
+             }
+          } else {
+              stack.push(str[i])
+          }
+      }
+    return (stack.length === 0)
+}
